82420b2c5196f18bbc3d3ca296c8e934f900cc49
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-06-12 21:42:50 -0300

    Hacemos pruebas con el método read() del objeto in

---
+++ u2/src/conceptos/EntradaPrimitiva.java
@@ -0,0 +1,18 @@
+package conceptos;
+
+import java.io.IOException;
+
+public class EntradaPrimitiva {
+
+    public static void main(String[] args) throws IOException {
+
+        int num = System.in.read();
+        System.out.println(num);
+        num = System.in.read();
+        System.out.println(num);
+        num = System.in.read();
+        System.out.println(num);
+        num = System.in.read();
+        System.out.println(num);
+    }
+}
