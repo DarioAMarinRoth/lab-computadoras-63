56563ab92266ead9b43a86c86ece3cc38a6d4564
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-06-12 21:36:49 -0300

    hacemos nuestro primer programa en Java

---
+++ u2/src/ejemplos/HolaMundo.java
@@ -0,0 +1,9 @@
+package ejemplos;
+
+public class HolaMundo {
+    public static void main(String[] args) { // -> atajo: psvm + tab
+        System.out.println("Laboratorio de computadoras");
+        System.out.print("Laboratorio de computadoras electrónicas");
+        System.out.println();
+    }
+}
