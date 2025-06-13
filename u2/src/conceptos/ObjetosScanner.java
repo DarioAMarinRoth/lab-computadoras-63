20ce0a57069357d732e4a4f36d9fa6b7562b5695
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-06-12 21:41:39 -0300

    analizamos las funcionalidades de la clase Scanner

---
+++ u2/src/conceptos/ObjetosScanner.java
@@ -0,0 +1,36 @@
+package conceptos;
+
+import java.util.Scanner;
+
+public class ObjetosScanner {
+    public static void main(String[] args) {
+
+        String cuentito = "3 tristes tigres \n tragaban trigo \n en un trigal.";
+
+        Scanner miScanner = new Scanner(cuentito);
+
+//        int num = miScanner.nextInt();
+//        System.out.println(num);
+//        String palabra = miScanner.next();
+//        System.out.println(palabra);
+//        palabra = miScanner.next();
+//        System.out.println(palabra);
+//        num = miScanner.nextInt();
+//        System.out.println(num);
+
+        System.out.println(miScanner.nextLine());
+        System.out.println(miScanner.nextLine());
+        System.out.println(miScanner.nextLine());
+
+        miScanner.close();
+
+        // Métodos útiles de Scanner:
+
+        // next() -> lee la siguiete palabra.
+        // nextLine() -> lee la siguiente línea.
+        // nextInt() -> lee el siguiente entero.
+        // nextBoolean() -> lee el siguiente booleano
+        // nextDouble() -> lee el siguiente double(flotante).
+
+    }
+}
