00ec1ee67a5c5918ba04ca12a29a1415ac1bf226
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-06-12 21:37:58 -0300

    vemos los distintos tipos de comentarios

---
+++ u2/src/conceptos/Comentarios.java
@@ -0,0 +1,15 @@
+package conceptos;
+
+public class Comentarios {
+
+    // Esto es un comentario de una línea. -> ctrl + /
+
+    /*
+     Esto es
+     un comentario
+     de varias -> ctrl + shift + /
+     líneas
+    */
+
+}
+
