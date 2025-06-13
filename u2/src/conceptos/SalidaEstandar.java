653623481fae7edeb89e351a68736a9d90d85b13
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-06-12 21:39:17 -0300

    analizamos los diferentes métodos del objeto out

---
+++ u2/src/conceptos/SalidaEstandar.java
@@ -0,0 +1,9 @@
+package conceptos;
+
+public class SalidaEstandar {
+    public static void main(String[] args) {
+        System.out.print("Mensaje");   // Imprime un mensaje en la consola.
+        System.out.println("Mensaje"); // Imprime un mensaje y provoca un salto de línea.
+        System.out.printf("Mensaje");  // Imprime un mensaje con un formato especificado. Lo vamos a usar en la unidad 3.
+    }
+}
