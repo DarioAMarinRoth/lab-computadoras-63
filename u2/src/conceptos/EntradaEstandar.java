762c71e740718c79c55988292ad147089473c68c
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-06-12 21:43:29 -0300

    vemos como recibir entradas de teclado con la ayuda de un objeto del tipo Scanner

---
+++ u2/src/conceptos/EntradaEstandar.java
@@ -0,0 +1,15 @@
+package conceptos;
+
+import java.util.Scanner;
+
+public class EntradaEstandar {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        Scanner otroScanner = new Scanner(System.in);
+
+        System.out.print("Por favor, ingrese un número: ");
+        int num = teclado.nextInt();
+        System.out.println("El número ingresado fue " + num);
+    }
+}
