ddda2b4e4d07f37bf7a93ef85ea8bf7d3e2adc5d
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-08-30 11:32:10 -0300

    impementamos una función para imprimir matrices de enteros

--- u3/src/utils/Funciones.java
+++ u3/src/utils/Funciones.java
@@ -1,4 +1,13 @@
 package utils;
 
 public class Funciones {
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.printf("%-10d ", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
