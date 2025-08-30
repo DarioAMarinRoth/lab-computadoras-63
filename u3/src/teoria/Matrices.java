0a16e6ae76851198f73c18002a5532af295667a6
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-08-30 11:31:44 -0300

    vemos arrays de dos dimensiones (matrices)

--- u3/src/teoria/Matrices.java
+++ u3/src/teoria/Matrices.java
@@ -1,4 +1,21 @@
 package teoria;
 
+import static utils.Funciones.printMatrix;
+
 public class Matrices {
+    public static void main(String[] args) {
+//        int[][] unArray = new int[5][3];
+//        unArray[3][1] = 4;
+//
+//        System.out.println(unArray[0].length);
+//
+//        // un vector de enteros
+//
+////        int[] miVector = new int[10];
+////
+////        System.out.println(miVector.length);
+
+        int[][] miMatriz = {{1, 0, 0},{0, 1, 100},{1000, 2, 4},{0, 1000000, 5}};
+        printMatrix(miMatriz);
+    }
 }
